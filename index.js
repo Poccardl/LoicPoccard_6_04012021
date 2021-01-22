7320787064fafef371f4d3a0a51b7a04cd1c87eb
Poccardl <loic.poccard@gmail.com>
2021-01-22 15:30:31 +0100

    structure updated for index.js / add comments in js code

--- index.js
+++ index.js
@@ -1,34 +1,11 @@
-function FetchTag() {
-    // Récupère le tag des photographes à afficher à l'aide d'une expression régulière
-    const regex_tag = RegExp(/\w{0,}$/)
-    const photographer_tag = regex_tag.exec(document.URL)[0]
-    console.log("photographer_tag :", photographer_tag)
-    return photographer_tag
-}
-var type = FetchTag()
-
 FetchEvents()
+let type = FetchTag()
 FetchPhotographersData(type)
 
-//TODO: add commentaire
-function FetchPhotographersData(tag) {
-    fetch("./data/FishEyeDataFR.json")
-    .then(function(resp) {
-        return resp.json()
-    })
-    .then(function(data) {
-        if (tag == "html") {
-            tag = "all"
-        }
-        console.log("photographers Data ->", data["photographers"])
-        RemovePhotographers()
-        PhotographersFactory(data["photographers"], tag)
-        FetchEvents()
-    })
-}
-
-//TODO: add commentaire
 function FetchEvents() {
+    /* Permet de récupérer les différents noeuds dans le DOM du fichier "index.html".
+    Mais aussi  d'ajouter des Events */
+
     // DOM Elements
     const portailNavigation = document.querySelectorAll(".portrait")
     const artNavigation = document.querySelectorAll(".art")
@@ -51,75 +28,14 @@ function FetchEvents() {
     eventsNavigation.forEach((link) => link.addEventListener("click", FilterPhotographers_events));
 }
 
-//TODO: add commentaire
-function PhotographersFactory(data, tag) {
-    for (var element in data) {
-        if (data[element].tags.includes(tag)) {
-            var data_photographers = {
-                name: data[element].name,
-                id: data[element].id,
-                city: data[element].city,
-                country: data[element].country,
-                tags: data[element].tags,
-                tagline: data[element].tagline,
-                price: data[element].price,
-                portrait: data[element].portrait,
-            }
-        }
-        else {
-            continue
-        }
-        AddPhotographersCards(data_photographers)
-    }
+function FetchTag() {
+    /* Récupère le tag des photographes à afficher à l'aide d'une expression régulière */
+    const regex_tag = RegExp(/\w{0,}$/)
+    const photographer_tag = regex_tag.exec(document.URL)[0]
+    return photographer_tag
 }
 
-//TODO: add commentaire
-function AddPhotographersCards(data_photographers) {
-    // sélectionne la section #photographer_section
-    const photographersSection = document.getElementById("photographer_section") // TODO: à un autre endroit dans le code ??
-
-    let photographers_card_html = '<div class="card">' +
-    '<a class="photographers_link" href="photographers-page.html?id=' + data_photographers["id"] + '"><img src="data/Photographers ID Photos/' + data_photographers["portrait"] + '" alt="" class="photographers_pp"></a>' +
-    '<h2>' + data_photographers["name"] + '</h2>' +
-    '<div class="content">' +
-    '<p class="location">' + data_photographers["city"] + ', ' + data_photographers["country"] + '<p>' +
-    '<p>' + data_photographers["tagline"] + '</p>' +
-    '<p>' + data_photographers["price"] + '€/jour</p>' +
-    '<nav class="index_nav" role="navigation" aria-label="photographer categories">' +
-    '<ul>'
-    // ajoute les tags pour la navigation
-    var photographers_tags_html = ""
-    for (var element in data_photographers["tags"]) {
-        if (data_photographers["tags"][element] != "all") {
-            photographers_tags_html = photographers_tags_html +
-            '<li class="' + data_photographers["tags"][element] + '">#' +
-            data_photographers["tags"][element] +
-            '</li>'
-        }
-    }
-    photographers_card_html = photographers_card_html +
-    photographers_tags_html +
-    '</ul>' +
-    '</nav>' +
-    '</div>' +
-    '</div>'
-
-    // ajoute les cartes des différents photographes dans le code HTML
-    photographersSection.insertAdjacentHTML("afterbegin", photographers_card_html)
-}
-//TODO: add commentaire
-function RemovePhotographers() {
-    try {
-        const photographersSection = document.querySelectorAll(".card")
-        for (var element in photographersSection) {
-            photographersSection[element].remove()
-        }
-    } catch (error){
-        console.error("RemovePhotographers : failed")
-    }
-}
-
-
+/* Permet de définir quelles photographes afficher en fonction du tag sélectionné */
 function FilterPhotographers_portrait() {
     FetchPhotographersData("portrait")
 }
@@ -145,7 +61,93 @@ function FilterPhotographers_events() {
     FetchPhotographersData("events")
 }
 
+function FetchPhotographersData(type) {
+    /* Récupère les données sur les différents photographes dans le fichier "FisheEyeDataFR.json".
+    La fonction attend un argument non optionnel -> type(String) */
+    fetch("./data/FishEyeDataFR.json")
+    .then(function(resp) {
+        return resp.json()
+    })
+    .then(function(data) {
+        if (type == "html") {
+            type = "all"
+        }
+        RemovePhotographers()
+        PhotographersFactory(data["photographers"], type)
+        FetchEvents()
+    })
+}
+
+function PhotographersFactory(data, type) {
+    /* Créer un dictionnaire data_photographers{} pour ensuite appeler la function  AddPhotographersCards() en lui donnant data_photographers{} en paramètre
+    La fonction attend deux arguments non optionnel -> data{} et type(String) */
+    let data_photographers = {}
+    for (let element in data) {
+        // permet de filtrer les photographes à afficher en fonction de leurs tags
+        if (data[element].tags.includes(type)) {
+            data_photographers = {
+                name: data[element].name,
+                id: data[element].id,
+                city: data[element].city,
+                country: data[element].country,
+                tags: data[element].tags,
+                tagline: data[element].tagline,
+                price: data[element].price,
+                portrait: data[element].portrait,
+            }
+        }
+        else {
+            continue
+        }
+        AddPhotographersCards(data_photographers)
+    }
+}
+
+function AddPhotographersCards(data_photographers) {
+    /* Ajoute les cartes html des différents photographes à la page index.html
+    La fonction attend un argument non optionnel -> data_photographers{} */
+    const photographersSection = document.getElementById("photographer_section")
+    let photographers_card_html = `<div class="card">
+    <a class="photographers_link" href="photographers-page.html?id=${data_photographers["id"]}"><img src="data/Photographers ID Photos/${data_photographers["portrait"]}" alt="" class="photographers_pp"></a>
+    <h2>${data_photographers["name"]}</h2>
+    <div class="content">
+    <p class="location">${data_photographers["city"]},${data_photographers["country"]}<p>
+    <p>${data_photographers["tagline"]}</p>
+    <p>${data_photographers["price"]} €/jour</p>
+    <nav class="index_nav" role="navigation" aria-label="photographer categories">
+    <ul>`
+    // ajoute les tags pour la navigation
+    let photographers_tags_html = ""
+    for (let element in data_photographers["tags"]) {
+        if (data_photographers["tags"][element] != "all") {
+            photographers_tags_html = photographers_tags_html +
+            '<li class="' + data_photographers["tags"][element] + '">#' +
+            data_photographers["tags"][element] +
+            '</li>'
+        }
+    }
+    photographers_card_html += `${photographers_tags_html}
+    </ul>
+    </nav>
+    </div>
+    </div>`
+    photographersSection.insertAdjacentHTML("afterbegin", photographers_card_html)
+}
+
+function RemovePhotographers() {
+    /* Supprime les cartes des photographes du code html */
+    try {
+        const photographersSection = document.querySelectorAll(".card")
+        for (var element in photographersSection) {
+            photographersSection[element].remove()
+        }
+    } catch {
+        //
+    }
+}
+
 function ScrollLink() {
+    /* Permet d'afficher le button "Passer au contenu" lors d'un scroll vers le bas de la page index.html */
     const scrollLink = document.querySelector(".dynamic_display")
     if (window.scrollY > 20) {
         scrollLink.style.display = "block"
